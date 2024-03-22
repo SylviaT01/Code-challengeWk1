5044448373caf2310238884ebb62d23ffb29b866
Sylvia <sylviachebet03@gmail.com>
2024-03-22 14:51:11 +0300

    add script.js

---
+++ speed-detector/script.js
@@ -0,0 +1,17 @@
+function calculatePoints() {
+    let spd = parseInt(document.querySelector("#spd").value);
+    let speedLimit = 70;
+    let demeritsPer5km = 1;
+    let maxDemeritPoints = 12;
+
+    if (spd <= speedLimit) {
+        document.querySelector("#result").innerText = "Ok";
+    } else {
+        const Points = Math.floor((spd - speedLimit) / 5) * demeritsPer5km;
+        if (Points >= maxDemeritPoints) {
+            document.querySelector("#result").innerText = "License suspended";
+        } else {
+            document.querySelector("#result").innerText = "Points: " + Points;
+        }
+    }
+}
